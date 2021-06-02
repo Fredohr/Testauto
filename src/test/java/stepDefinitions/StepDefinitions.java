cb8f7d67411a9f00147890aca075c3c60258845a
fredohr <fredohr@outlook.com>
2021-06-02 20:28:30 +0200

    New Commit

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,7 +18,9 @@ public class StepDefinitions {
 
 	WebDriver driver;
 	Random random = new Random(100);
+	randomString.RandomString Rstring = new randomString.RandomString();
 
+	
 	@Given("I want navigate to https:\\/\\/login.mailchimp.com\\/signup\\/")
 	public void i_want_navigate_to_https_login_mailchimp_com_signup() {
 
@@ -52,52 +53,70 @@ public class StepDefinitions {
 	public void i_create_a_larsipan2_and_press_sign_up() {
 
 		WebElement searchBox11 = driver.findElement(By.name("password"));
-		searchBox11.sendKeys("Larsipan2!");
+		searchBox11.sendKeys("Larsipan244!");
 		searchBox11.submit();
 
 	}
-	
 
-		@When("I register my hars@lars.com")
-		public void i_register_my_hars_lars_com() {
-			
-			WebElement searchBox = driver.findElement(By.name("email"));
-			searchBox.sendKeys("hars@lars.com");
-		  
-		}
+	@When("I register my hars@lars.com")
+	public void i_register_my_hars_lars_com() {
 
+		WebElement searchBox = driver.findElement(By.name("email"));
+		searchBox.sendKeys("hars@lars.com");
 
-		@When("I create a Hasse666")
-		public void i_create_a_hasse666() {
-			
-			WebElement searchBox1 = driver.findElement(By.name("username"));
-			searchBox1.sendKeys("Hasse676");
-			
-		}
-		@When("I Create a Larsipan1! and press sign up")
-		public void i_create_a_larsipan1_and_press_sign_up() {
-		    
-			WebElement searchBox11 = driver.findElement(By.name("password"));
-			searchBox11.sendKeys("Larsipan2!");
-			searchBox11.submit();
+	}
 
-			
-		}
+	@When("I create a Hasse666")
+	public void i_create_a_hasse666() {
 
+		WebElement searchBox1 = driver.findElement(By.name("username"));
+		searchBox1.sendKeys("Hasse676");
 
+	}
 
+	@When("I Create a Larsipan1! and press sign up")
+	public void i_create_a_larsipan1_and_press_sign_up() {
+
+		WebElement searchBox11 = driver.findElement(By.name("password"));
+		searchBox11.sendKeys("Larsipan1!");
+		searchBox11.submit();
+
+	}
 
 	@Then("I validate that i is re-directed to the confirmation page")
 	public void i_validate_that_i_is_re_directed_to_the_confirmation_page() {
 
-		WebElement ReEnter = driver.findElement(
-				By.className("signup-wrap"));
+		WebElement ReEnter = driver.findElement(By.className("signup-wrap"));
 		boolean ReEnterDisplayed = ReEnter.isDisplayed();
 		assertEquals(true, ReEnterDisplayed);
 
 	}
 	
+		@When("I register my lazz@lars.com")
+		public void i_register_my_lazz_lars_com() {
+			
+			WebElement searchBox = driver.findElement(By.name("email"));
+			searchBox.sendKeys(random+"@lars.com");
+		   
+		}
+
+		@When("I create a ")
+		public void i_create_a() {
+			
+			WebElement searchBox1 = driver.findElement(By.name("username"));
+			searchBox1.sendKeys("lars"+Rstring+Rstring+Rstring+Rstring+Rstring);
+		    
+		}
+		@When("I Create a Larsipan3! and press sign up")
+		public void i_create_a_larsipan3_and_press_sign_up() {
+			
+			WebElement searchBox11 = driver.findElement(By.name("password"));
+			searchBox11.sendKeys("Larsipan3!");
+			searchBox11.submit();
+		   
+		}
+
+
+
+
 }
-
-
-
